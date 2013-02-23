7f406faa9315e7b3f0b6c0902bab49636fd6f44e
Paul Wallace <PaulWallacc@gmail.com>
2013-02-23 18:06:05 -0500

    First Edits

--- gen/org/teamrubiconusa/teamrubicon/R.java
+++ gen/org/teamrubiconusa/teamrubicon/R.java
@@ -22,9 +22,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int splash2=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080004;
+        public static final int background_image=0x7f080003;
         public static final int pager=0x7f080000;
         public static final int pager_title_strip=0x7f080001;
         public static final int section_label=0x7f080002;
@@ -32,6 +34,7 @@ public final class R {
     public static final class layout {
         public static final int activity_team_rubicon=0x7f030000;
         public static final int fragment_team_rubicon_dummy=0x7f030001;
+        public static final int splash_screen=0x7f030002;
     }
     public static final class menu {
         public static final int team_rubicon=0x7f070000;
